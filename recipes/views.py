0f8fa41bab96029fb0c2566d192054452e32c882
Maria Andreza C Santos <mandreza008@gmail.com>
2024-09-17 23:43:13 -0300

    partials

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
 
 
 '''def sobre(request):
