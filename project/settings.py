9c7c88fb0cda231837f6dc7d962e264d647a23f7
Maria Andreza C Santos <mandreza008@gmail.com>
2024-09-25 22:44:31 -0300

    files statics

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,11 @@ STATICFILES_DIRS = [
 
 ]
 
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
