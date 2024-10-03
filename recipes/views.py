7c035496f055e4d259cc77cfa5353671a9ad1efe
Maria Andreza C Santos <mandreza008@gmail.com>
2024-10-02 22:11:52 -0300

    fix(funcionalidade): gerador de recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,25 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': make_recipe(),
     })
-
-
-'''def sobre(request):
-    return render(request, 'temp/temp.html')
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')'''
