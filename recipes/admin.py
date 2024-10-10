00c4baa409cc16d4c46b170feb9fa869a1a42930
Maria Andreza C Santos <mandreza008@gmail.com>
2024-10-09 23:08:31 -0300

    fix:(funcionalidade) Adiciona Funcionaliadade Categoria

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin): 
+    ...
+
+
+
+admin.site.register(Category, CategoryAdmin)
+
