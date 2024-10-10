f57dcdbe33dcf6e5fdd10d7d0122e44e96d1090d
Maria Andreza C Santos <mandreza008@gmail.com>
2024-10-09 22:12:05 -0300

    fix:(app-names) urls dinamicas

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from . import views
 
-
-# HTTP REQUEST <- HTTP RESPONSE
+# recipes:recipe
+app_name = "recipes"
 
 urlpatterns = [
-    path('', views.home), 
-    path('recipes/<int:id>/', views.recipe), 
-    
+    path('', views.home, name="home"), 
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
+
 
 ]
