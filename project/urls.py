dfcb0d967a6278c441e5e5d05c88e16410916b0d
Maria Andreza C Santos <mandreza008@gmail.com>
2024-10-09 23:52:29 -0300

    config urls e models

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 # HTTP REQUEST <- HTTP RESPONSE
@@ -25,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')), # /
     # path('recipes/', include('recipes.urls')), # dominio.com/recipes/
-    
-
 ]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- recipes/models.py
+++ recipes/models.py
@@ -46,3 +46,6 @@ class Recipe(models.Model):
         User, on_delete=models.SET_NULL, null=True
     )
 
+    def __str__(self):
+        return self.title
+
